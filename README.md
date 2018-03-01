4c919c39d064ce9c4eab74690985b7812083e268
WilkerS1 <walkerxr@hotmail.com>
2018-02-28 22:16:26 -0300

    first commit, added project

---
+++ README.md
@@ -0,0 +1,6 @@
+#	etecSQL
+####	TI 2º módulo
+######	iniciado no dia 28-02-2018(quinta-feira)
+
+__pt-br:__	SQL vem aqui.  
+__en:__		SQL goes here.
---
+++ SQLQuery1.sql
@@ -0,0 +1,42 @@
+use master;						--disuse master
+drop database		bancho		--delete bancho
+create database		bancho		--recreate bancho*
+use					bancho		--use bancho
+go								--go to bancho
+/*
+	*Bancho is a reference to the name of the official Osu! servers
+*/
+
+
+create table tb_clients (
+	id_client				int,
+	client_name				varchar,
+	client_address			varchar,
+	client_phone			int,
+	client_email			varchar,
+)
+
+create table tb_sell_listitems (
+	id_item					int,
+	item_desc				text,
+	item_price				float,
+	item_unitqtd			int,
+)
+
+create table tb_sell_checkouts (
+	id_checkout				int,
+	id_client				int,
+	checkout_date			date,
+	checkout_value_total	float,
+	checkout_value_discount	float,
+	checkout_value_paid		float,
+)
+
+create table tb_sell_checkouts_items (
+	id_checkout_item		int,
+	id_checkout				int,
+	id_item					int,
+	checkout_item_qtd		int,
+	checkout_item_value		float,
+)
+
