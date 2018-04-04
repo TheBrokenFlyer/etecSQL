ac97c37c6d6e9250921d13e02ed3bd11ad9a077f
WilkerS1 <walkerxr@hotmail.com>
2018-04-03 22:38:50 -0300

    e

--- TLBD.sql
+++ TLBD.sql
@@ -103,10 +103,11 @@ insert into tb_sell_checkouts(id_client,checkout_date,checkout_value_paid,/**/ch
 	
 
 /* reading values from a table :3c */
---select * from tb_clients			--select everything (*) from tb_clients
+select * from tb_clients			--select everything (*) from tb_clients
 --select * from tb_listitems
 --select * from tb_sell_checkouts
 
---select client_name,client_phone from tb_clients --select client_name and client_phone, in this order to show
---select client_name				from tb_clients order by id_client desc --order by ID in decrescent order
-select client_name from tb_clients where client_name like '%m%' --"like" looks for condicions. % works like * thus selecting everything
+--select client_name,client_phone	from tb_clients									--select client_name and client_phone, in this order to show
+--select client_name				from tb_clients order by id_client desc			--order by ID in decrescent order
+select *							from tb_clients where client_name like '%m%'	--"like" looks for condicions. % works like * thus selecting everything
+select *							from tb_clients where client_phone < 300000		
