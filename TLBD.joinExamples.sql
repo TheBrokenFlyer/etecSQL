07a92558c8c9509be613dc710b5d5aef053772a5
WilkerS1 <walkerxr@hotmail.com>
2018-05-01 22:44:14 -0300

    edit and more clear

--- TLBD.joinExamples.sql
+++ TLBD.joinExamples.sql
@@ -10,36 +10,38 @@ create table tbB (
 	name	varchar(32)	null
 );
 
-insert into tbA(name)	values('fernanda');
-insert into tbA(name)	values('luiz');
-insert into tbA(name)	values('fernando');
-insert into tbA(name)	values('josefa');
+insert into tbA(name) values
+	('fernanda'),
+	('luiz'),
+	('fernando'),
+	('josefa');
 
 
-insert into tbB(name)	values('carlos');
-insert into tbB(name)	values('fernando');
-insert into tbB(name)	values('manoel');
-insert into tbB(name)	values('luiz');
+insert into tbB(name) values
+	('carlos'),
+	('fernando'),
+	('manoel'),
+	('luiz');
 
 
-
-select AName.name, BName.name	--select colums from both tables 
-	from		tbA			as	AName	--takes A as the name "AName"
-	inner join	tbB			as	BName	--takes B as the name "BName"
-			on	AName.name	=	BName.name	--takes B and compare if there are registries in B that are equals to A. shows compatible values.
 
 select AName.name, BName.name	--select colums from both tables 
-	from		tbA			as	AName	--takes A as the name "AName"
-	left join	tbB			as	BName	--takes B as the name "BName"
-			on	AName.name	=	BName.name	--takes B and compare if there are registries in B that are equals to A. shows if true. NULL if false.
+	from		tbA			as	AName	--takes table A as the name "AName"
+	inner join	tbB			as	BName	--takes table B as the name "BName"
+			on	AName.name	=	BName.name	--takes B and compare if there are registries in A that are equals to B. shows compatible values.
 
 select AName.name, BName.name	--select colums from both tables 
-	from		tbA			as	AName	--takes A as the name "AName"
-	left join	tbB			as	BName	--takes B as the name "BName"
-			on	AName.name	=	BName.name	--takes B and compare if there are registries in B that are equals to A. shows if true. NULL if false.
+	from		tbA			as	AName	--takes table A as the name "AName"
+	left join	tbB			as	BName	--takes table B as the name "BName"
+			on	AName.name	=	BName.name	--takes A and compare if there are registries in A that are equals to B. shows values if true. NULL if false.
 
 select AName.name, BName.name	--select colums from both tables 
-	from		tbA			as	AName	--takes A as the name "AName"
-	left join	tbB			as	BName	--takes B as the name "BName"
+	from		tbA			as	AName	--takes table A as the name "AName"
+	right join	tbB			as	BName	--takes table B as the name "BName"
+			on	BName.name	=	AName.name	--takes B and compare if there are registries in B that are equals to A. shows values if true. NULL if false.
+
+select AName.name, BName.name	--select colums from both tables 
+	from		tbA			as	AName	--takes table A as the name "AName"
+	left join	tbB			as	BName	--takes table B as the name "BName"
 			on	AName.name	=	BName.name	--takes A and looks for registries in B. if compatible, shows value. if not, NULL is shown and shows incompatible registries at the last lines
 
