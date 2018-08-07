e0914634c67cd77cbd6ad08e69cb46fb4194b921
WilkerS1 <walkerxr@hotmail.com>
2018-08-07 20:41:55 -0300

    stuff changed

--- TLBD.revisao.sql
+++ TLBD.revisao.sql
@@ -6,6 +6,13 @@ use dtbSql;
 --
 --TABELAS
 --
+create table tbMetodologias (
+	idMetodo int NOT NULL identity(0,1),
+	metodoNome varchar(16) NOT NULL,
+	
+	primary key (idMetodo),
+)
+
 create table tbTarefa(
 	idTarefa int NOT NULL identity(0,1),
 	tarefaTitulo varchar(32) NOT NULL unique,
@@ -13,11 +20,13 @@ create table tbTarefa(
 	tarefaPrazo date NOT NULL,
 	tarefaPrazoInicio date,
 	tarefaPrazoTermino date,
+	idMetodo int,
 	
-	primary key (idTarefa)
+	primary key (idTarefa),
+	foreign key (idMetodo) references tbMetodologias(idMetodo),
 )
 
-create table tbPessoas (
+create table tbPessoa (
 	idPessoa int NOT NULL identity(0,1),
 	pessoaNome varchar(32) NOT NULL,
 	pessoaEmail varchar(48) NOT NULL unique,
@@ -25,18 +34,55 @@ create table tbPessoas (
 	primary key (idPessoa),
 )
 
-create table tbParticipantes_TarefaPessoas (
+create table tbRef_TarefaPessoa (
 	idParticipante int NOT NULL identity(0,1),
 	idTarefa int NOT NULL,
 	idPessoa int NOT NULL,
 	
 	foreign key (idTarefa) references tbTarefa(idTarefa),
-	foreign key (idPessoa) references tbPessoas(idPessoa),
+	foreign key (idPessoa) references tbPessoa(idPessoa),
 )
 
-create table tbMetodologias (
-	idMetodo int NOT NULL identity(0,1),
-	metodoNome varchar(16) NOT NULL,
+--
+--VALORES
+--
+insert into tbTarefa(tarefaTitulo,tarefaDesc,tarefaPrazo) values
+	('Tarefa sobre ''tal''', 'Esta tarefa se refere a ''tal'' coisa pra ser feita assim.', '2018-08-07'),
+	('Esta outra coisa', 'Isso aqui é isso.', '2018-03-31');
+
+insert into tbPessoa(pessoaNome, pessoaEmail) values
+	('Frisk', 'person@somewhere'),
+	('Fool', 'aaa@at'),
+	('Bar', 'noOne@nowhere');
+
+insert into tbRef_TarefaPessoa(idTarefa,idPessoa) values
+	(0,0),
+	(1,1),
+	(1,2);
+
+insert into tbMetodologias(metodoNome) values
+	('comodoro'),
+	('scram'),
+	('someOtherMethod'),
+	('idkWhatAreThose'),
+	('eeeeeeee');
+
+--
+--SELEÇÕES
+--
+select
+	t.tarefaTitulo as [Título],
+	t.tarefaDesc as [Descrição],
+	t.tarefaPrazo as [Prazo]
+	from tbTarefa as t;
 	
-	primary key (idMetodo),
-)
+select
+	p.pessoaNome as [Nome],
+	p.pessoaEmail as [Email]
+	from tbPessoa as p;
+	
+select * from tbRef_TarefaPessoa;
+
+select
+	m.metodoNome as [Métodos]
+	from tbMetodologias as m;
